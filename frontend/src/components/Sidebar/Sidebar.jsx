cb448ec7e8778fd5e359d2351ebb98ff5c9340e5
Geeta Mistry <geeta.mistry2001@gmail.com>
2026-07-11 23:25:08 +0530

    Refactor Sidebar component for improved layout and styling

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -8,12 +8,32 @@ const Sidebar = () => {
             <h1 className='text-2xl font-bold'>BillPro</h1>
             <p className='text-sm text-slate-300'>Bill Management System</p>
         </div>
-        <div>
+        <div className='px-3 font-medium'>
             <ul >
-                <li className='flex items-center gap-1 mt-4 px-4 hover:bg-slate-800 cursor-pointer rounded-lg '>
+                <li className='flex items-center gap-2 mt-4  px-2 py-2 hover:bg-slate-700 cursor-pointer rounded-lg '>
                     <MdDashboard />
                     Dashboard
                 </li>   
+                <li className='flex items-center gap-2  px-2 py-2 hover:bg-slate-700 cursor-pointer rounded-lg'>
+                    <FaBox />
+                    Products
+                </li>
+                <li className='flex items-center gap-2 px-2 py-2 hover:bg-slate-700 cursor-pointer rounded-lg'>
+                    <FaUsers />
+                    Customers
+                </li>
+                <li className='flex items-center gap-2 px-2 py-2 hover:bg-slate-700 cursor-pointer rounded-lg'>
+                    <FaFileInvoiceDollar />
+                    Bills
+                </li>
+                <li className='flex items-center gap-2 px-2 py-2 hover:bg-slate-700 cursor-pointer rounded-lg'>
+                    <FaChartBar/>
+                    Reports
+                </li>
+                <li className='flex items-center gap-2 px-2 py-2 hover:bg-slate-700 cursor-pointer rounded-lg'>
+                    <FaCog />
+                    Settings
+                </li>
             </ul>
         </div>
         <div>logout</div>
