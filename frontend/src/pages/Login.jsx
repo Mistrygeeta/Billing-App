8b9c5841bdbfa2636341f0ea271cc44ccae14c56
Geeta Mistry <geeta.mistry2001@gmail.com>
2026-07-15 12:20:29 +0530

    Enhance Login component with password visibility toggle and email/password icons

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { FaCheckCircle } from "react-icons/fa";
+import { FaCheckCircle, FaEnvelope,FaLock,FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 
 
 const Login = () => {
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("")
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
     const navigate = useNavigate()
 
@@ -61,18 +62,27 @@ const Login = () => {
        </div>
        </div>
        <div className="w-1/2 flex justify-center items-center ">
-        <div className="bg-white rounded-2xl shadow-2xl p-10 w-[420px]">
+        <div className="bg-white rounded-2xl shadow-2xl p-10 w-[460px]">
             <h2 className="text-3xl font-bold text-gray-800">Welcome Back</h2>
             <p className="text-gray-500 mt-2">Sign in to continue to BillPro</p>
         <div className="mt-10">
           <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-2 ">Email</label>
+          <div className="relative">
+            <FaEnvelope className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400" />
           <input type="email" id="email" placeholder="Enter your email" value={email} onChange={handleEmailChange}
-          className="w-full border border-gray-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-slate-900" />
+          className="w-full border border-gray-300 rounded-lg  py-3 pl-12 pr-4 outline-none focus:ring-2 focus:ring-slate-900" />
+        </div>
         </div>
         <div className="mt-6">
           <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-2">Password</label>
-          <input type="password" id="password" placeholder="Enter your password" value={password} onChange={handlePasswordChange} 
-          className="w-full border border-gray-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-slate-900"/>
+          <div className="relative">
+            <FaLock className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400"/>
+          <input type={showPassword ? "text" : "password"} id="password" placeholder="Enter your password" value={password} onChange={handlePasswordChange} 
+          className="w-full border border-gray-300 rounded-lg py-3 pl-12 pr-12 outline-none focus:ring-2 focus:ring-slate-900"/>
+           {showPassword ?(<FaEyeSlash onClick={()=> setShowPassword(false)} className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 cursor-pointer"/>
+          ):(
+          <FaEye onClick={()=> setShowPassword(!showPassword)} className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 cursor-pointer" />)}
+        </div>
         </div>
         <div className="flex justify-between items-center mt-4">
           <div className="flex items-center gap-2" >
@@ -82,7 +92,12 @@ const Login = () => {
           <Link to="/forgot-password" className="text-sm text-slate-900 font-medium hover:underline">Forgot Password?</Link>
         </div>
         <button onClick={handleLogin} className="w-full bg-slate-900 text-white py-3 rounded-lg font-semibold mt-6 hover:bg-slate-700 transition duration-300">Login</button>
+        <div className="mt-8 text-center">
+          <p className="text-sm text-gray-600">Don't have an account?{" "} 
+            <Link to="/signup" className="text-slate-900 font-semibold hover:underline">Sign Up</Link> </p>
         </div>
+        </div>
+        
     </div>
     </div>
   )
