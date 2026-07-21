90dd0d3a9fa4bc796a4725616e85298c4c78ad49
Geeta Mistry <geeta.mistry2001@gmail.com>
2026-07-21 20:43:22 +0530

    Implement customer search functionality and add modal for adding new customers

--- frontend/src/pages/Customers.jsx
+++ frontend/src/pages/Customers.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar/Sidebar';
 import Navbar from '../components/Navbar/Navbar';
 import { FaPlus, FaSearch } from 'react-icons/fa';
 
 const Customers = () => {
+  const [search, setSearch] = useState("");
+  const [showModal, setShowModal] = useState(false)
+  const [customers, setCustomers] = useState([{
+     name: "Rahul Sharma",
+    phone: "9876512340",
+    email: "rahul123@gmail.com",
+    address: "New Delhi",
+  },
+  {
+     name: "Ram Sharma",
+    phone: "9886512340",
+    email: "ram123@gmail.com",
+    address: "New Delhi",
+  }
+]);
+
+  const filteredCustomers = customers.filter((customer)=>
+  customer.name.toLowerCase().includes(search.toLowerCase())||
+  customer.phone.includes(search))
+  
   return (
     <div className='flex min-h-screen'>
       <Sidebar/>
@@ -15,11 +35,11 @@ const Customers = () => {
           <div className='flex justify-between items-center mt-8'>
             <div className='relative'>
               <FaSearch className='absolute top-1/2 left-3 -translate-y-1/2 text-gray-400'/>
-                <input type="text" placeholder='Search Customer'
+                <input type="text" placeholder='Search Customer' value={search} onChange={(e)=> setSearch(e.target.value)}
                 className='border border-gray-300 py-2 pl-10 pr-4 w-80 rounded-lg outline-none' />
             </div>
             <div>
-              <button className='bg-slate-900 text-white rounded-lg px-5 py-2 cursor-pointer flex items-center gap-2 hover:bg-slate-700 transition'>
+              <button onClick={()=>setShowModal(true)} className='bg-slate-900 text-white rounded-lg px-5 py-2 cursor-pointer flex items-center gap-2 hover:bg-slate-700 transition'>
                 <FaPlus/>
                 <span>Add Customer</span>
               </button>
@@ -29,26 +49,53 @@ const Customers = () => {
             <table className='w-full border-collapse'>
               <thead className='bg-gray-100'>
                 <tr className='border-b'>
-                  <th className='p-3 text-left font-semibol text-gray-700'>Customer Name</th>
-                  <th className='p-3 text-left font-semibol text-gray-700'>Phone</th>
-                  <th className='p-3 text-left font-semibol text-gray-700'>Email</th>
-                  <th className='p-3 text-left font-semibol text-gray-700'>Address</th>
-                  <th className='p-3 text-left font-semibol text-gray-700'>Action</th>
+                  <th className='p-3 text-left font-semibold text-gray-700'>Customer Name</th>
+                  <th className='p-3 text-left font-semibold text-gray-700'>Phone</th>
+                  <th className='p-3 text-left font-semibold text-gray-700'>Email</th>
+                  <th className='p-3 text-left font-semibold text-gray-700'>Address</th>
+                  <th className='p-3 text-left font-semibold text-gray-700'>Action</th>
                 </tr>
               </thead>
               <tbody>
-                <tr className='border-b'>
-                  <td>Rahul Sharma</td>
-                  <td>9876512340</td>
-                  <td>rahul123@gmail.com</td>
-                  <td>New Delhi</td>
-                  <td>Edit Delete</td>
+                {filteredCustomers.length > 0 ?(
+                filteredCustomers.map((customer, index)=>(
+                <tr key={index} className='border-b hover:bg-gray-50 transition'>
+                  <td className='p-3 text-gray-800'>{customer.name}</td>
+                  <td className='p-3 text-gray-800'>{customer.phone}</td>
+                  <td className='p-3 text-gray-800'>{customer.email} </td>
+                  <td className='p-3 text-gray-800'>{customer.address} </td>
+                  <td className='p-3'>
+                    <div className='flex gap-2'>
+                      <button className='bg-blue-500 text-white rounded-md px-3 py-1 hover:bg-blue-600 transition'>
+                        Edit
+                      </button>
+                      <button className='bg-red-500 text-white rounded-md px-3 py-1 hover:bg-red-600 transition'>Delete</button>
+                    </div>
+                  </td>
                 </tr>
+                ))
+              ):(
+                <tr>
+                  <td colSpan="5" className='text-center py-8 text-gray-500 font-medium'>No Customers Found</td>
+                </tr>
+              )}
               </tbody>
             </table>
           </div>
         </div>
       </div>
+      {showModal && (
+        <div className='fixed inset-0 bg-black/40 flex items-center justify-center'>
+          <div className='bg-white rounded-xl p-6 w-[500px] shadow-xl '>
+            <h2 className='text-2xl font-bold text-slate-900'>Add Customer</h2>
+            <p className='text-gray-500 mt-2'>Enter Customer Details</p>
+          <div className='mt-6'>
+            <label htmlFor="" className='block text-sm font-medium mb-2 text-gray-700'>Customer Name</label>
+            <input type="text" placeholder='Enter customer name' className='w-full border border-gray-300 rounded-lg px-4 py-2 outline-none focus:border-slate-900' />
+          </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
