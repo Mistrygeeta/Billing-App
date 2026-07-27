c7779c0fce2a25a4f8d519da3c76b9f70630c252
Geeta Mistry <geeta.mistry2001@gmail.com>
2026-07-27 19:30:53 +0530

    Enhance bill creation modal with additional input fields for customer details and product information

--- frontend/src/pages/Bills.jsx
+++ frontend/src/pages/Bills.jsx
@@ -44,7 +44,7 @@ const Bills = () => {
         </div>
         {showModal && (
           <div className='fixed inset-0 bg-black/40 flex justify-center items-center'>
-            <div className='bg-white rounded-xl w-[900px] p-6 shadow-xl'>
+            <div className='bg-white rounded-xl max-w-3xl w-full p-6 shadow-xl max-h-[90vh] overflow-y-auto'>
               <div className='flex justify-between items-center'>
                 <h2 className='text-2xl font-bold text-slate-900'>Create New Bill</h2>
                 <button onClick={()=> setShowModal(false)} className='text-2xl text-gray-500 hover:text-red-500'>
@@ -54,6 +54,47 @@ const Bills = () => {
               <p className='text-gray-500 mt-2'>
                 Create a new invoice for your customer.
               </p>
+              <div className='grid grid-cols-2 gap-5 mt-6' >
+                <div>
+                  <label className='block text-sm font-medium text-gray-700 mb-2'>Customer Name</label>
+                  <input type="text" placeholder='Enter customer name' 
+                  className='w-full border border-gray-300 rounded-lg px-4 py-2 outline-none focus:border-slate-900'/>
+                </div>
+                <div>
+                  <label className='block text-sm font-medium text-gray-700 mb-2'>Phone Number</label>
+                  <input type="text" placeholder='Enter phone number'
+                  className='w-full border border-gray-300 rounded-lg px-4 py-2 outline-none focus:border-slate-900' />
+                </div>
+              </div>
+              <div className='mt-6'>
+                <h3 className='text-lg font-semibold text-slate-900'>Products</h3>
+                <p className='text-sm text-gray-500 mt-1'>Add Products to this invoice.</p>
+              </div>
+              <div className='grid grid-cols-5 gap-5 mt-5 '>
+                <div>
+                  <label className='block text-sm font-medium text-gray-700 mb-2'>Product</label>
+                  <input type="text" placeholder='Product name' 
+                  className='w-full border border-gray-300 rounded-lg px-4 py-2 outline-none focus:border-slate-900'/>
+                </div>
+                <div>
+                  <label className='block text-sm font-medium text-gray-700 mb-2'>Qty</label>
+                  <input type="number" placeholder='1'
+                  className='w-full border border-gray-300 rounded-lg px-4 py-2 outline-none focus:border-slate-900' />
+                </div>
+                <div>
+                  <label className='block text-sm font-medium text-gray-700 mb-2'>Price</label>
+                  <input type="number" min="0" placeholder='0'
+                  className='w-full border border-gray-300 px-4 py-2 rounded-lg outline-none focus:border-slate-900' />
+                </div>
+                <div>
+                  <label className='block text-sm font-medium mb-2 text-gray-700'>Total</label>
+                  <input type="text"  value="Rs. 0" readOnly
+                  className='w-full border border-gray-300 bg-gray-100 px-4 py-2 rounded-lg outline-none focus:border-slate-900'/>
+                </div>
+                <div className='flex items-end'>
+                  <button className='w-full h-[42px] bg-slate-900 text-white rounded-lg py-2 hover:bg-slate-700 transition'>Add</button>
+                </div>
+              </div>
             </div>
           </div>
         )}
